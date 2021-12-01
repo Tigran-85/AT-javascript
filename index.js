bda7401ddecb628448a9ed782c49578815cc86e4
Tigran Poghosyan <tigranpoghosyan94@gmail.com>
2021-12-02 01:35:01 +0400

    Material-3-Data-Types

---
+++ index.js
@@ -0,0 +1,14 @@
+//1. addition
+console.log('hello' + true);
+console.log('hello' + 3);
+console.log(5 + false);
+
+//2. multiplication
+console.log('hello' * true);
+console.log('hello' * 5);
+console.log(5 * false);
+
+//3. division
+console.log('hello'/true);
+console.log('hello'/5);
+console.log(5/false);
